a70b2333a73d595b197b54cc0209db2bfb801373
AhmadFajrul25 <AhmadFajrulllll@gmail.com>
2025-01-02 18:02:37 +0700

    pesan

--- petugas/dashboard.php
+++ petugas/dashboard.php
@@ -1,11 +1,15 @@
 <div class="row">
    <div class="col-12 col-lg-3">
-        <div class="card radius-15 bg-voilet">
+        <div class="card radius-15 bg-violet">
             <div class="card-body">
                 <div class="d-flex align-items-center">
                     <?php 
-                    $query_masyarakat = mysqli_query($koneksi, "SELECT * FROM masyarakat") or die (mysqli_error($koneksi));
-                    $jumlah_masyarakat = $query_masyarakat->num_rows;
+                    if ($koneksi) {
+                        $query_masyarakat = mysqli_query($koneksi, "SELECT * FROM masyarakat") or die(mysqli_error($koneksi));
+                        $jumlah_masyarakat = $query_masyarakat->num_rows;
+                    } else {
+                        $jumlah_masyarakat = 0; // Jika koneksi gagal
+                    }
                     ?>
                     <div>
                         <h2 class="mb-0 text-white"><?php echo $jumlah_masyarakat; ?></h2>
@@ -17,7 +21,6 @@
                     <div>
                         <p class="mb-0 text-white">Jumlah Masyarakat</p>
                     </div>
-                   
                 </div>
             </div>
         </div>
@@ -28,8 +31,12 @@
             <div class="card-body">
                 <div class="d-flex align-items-center">
                     <?php 
-                    $query_petugas = mysqli_query($koneksi, "SELECT * FROM petugas") or die (mysqli_error($koneksi));
-                    $jumlah_petugas = $query_petugas->num_rows;
+                    if ($koneksi) {
+                        $query_petugas = mysqli_query($koneksi, "SELECT * FROM petugas") or die(mysqli_error($koneksi));
+                        $jumlah_petugas = $query_petugas->num_rows;
+                    } else {
+                        $jumlah_petugas = 0; // Jika koneksi gagal
+                    }
                     ?>
                     <div>
                         <h2 class="mb-0 text-white"><?php echo $jumlah_petugas; ?></h2>
@@ -41,7 +48,6 @@
                     <div>
                         <p class="mb-0 text-white">Jumlah Petugas</p>
                     </div>
-
                 </div>
             </div>
         </div>
@@ -52,8 +58,12 @@
             <div class="card-body">
                 <div class="d-flex align-items-center">
                     <?php 
-                    $query_pengaduan = mysqli_query($koneksi, "SELECT * FROM pengaduan") or die (mysqli_error($koneksi));
-                    $jumlah_pengaduan = $query_pengaduan->num_rows;
+                    if ($koneksi) {
+                        $query_pengaduan = mysqli_query($koneksi, "SELECT * FROM pengaduan") or die(mysqli_error($koneksi));
+                        $jumlah_pengaduan = $query_pengaduan->num_rows;
+                    } else {
+                        $jumlah_pengaduan = 0; // Jika koneksi gagal
+                    }
                     ?>
                     <div>
                         <h2 class="mb-0 text-white"><?php echo $jumlah_pengaduan; ?></h2>
@@ -65,7 +75,6 @@
                     <div>
                         <p class="mb-0 text-white">Jumlah Pengaduan</p>
                     </div>
-                   
                 </div>
             </div>
         </div>
@@ -77,12 +86,20 @@
     <div class="card radius-15">
         <div class="card-body">
             <div class="card-title">
+                <?php 
+                if (!isset($_SESSION['petugas'])) {
+                    echo "<h4 class='mb-0 text-danger'>Sesi Tidak Ditemukan!</h4>";
+                } else { 
+                ?>
                 <h4 class="mb-0">Selamat Datang, <?php echo $_SESSION['petugas']['nama_petugas']; ?></h4>
+                <?php } ?>
             </div>
             <hr/>
+            <?php if (isset($_SESSION['petugas'])) { ?>
             <p>Nama : <?php echo $_SESSION['petugas']['nama_petugas']; ?></p>
             <p>Username : <?php echo $_SESSION['petugas']['username']; ?></p>
             <p>Level : <?php echo strtoupper($_SESSION['petugas']['level']); ?></p>
+            <?php } ?>
         </div>
     </div>
 </div>
